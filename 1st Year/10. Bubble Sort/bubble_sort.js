366d93d3f3e4b99a995c7dc60d32c5a99224111f
Abhinav Anand <manasgmishra@gmail.com>
2020-10-26 13:33:45 +0530

    Update bubble_sort.js

--- 1st Year/10. Bubble Sort/bubble_sort.js
+++ 1st Year/10. Bubble Sort/bubble_sort.js
@@ -18,6 +18,5 @@ function bubble_Sort(arrayToSort){
     } while (swapp);
  return x; 
 }
-
-// Expected outcome is [ 3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1 ]
 console.log(bubble_Sort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
+// Expected outcome is [ 3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1 ]
