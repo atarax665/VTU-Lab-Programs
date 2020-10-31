86736e7aa6eb3186905a9e9cc28540c4212e2738
vguo <vguo@us.ibm.com>
2020-10-31 15:14:31 -0400

    fix format issue with code review

--- 1st Year/02. Roots of a quadratic equation/quadratic.py
+++ 1st Year/02. Roots of a quadratic equation/quadratic.py
@@ -10,7 +10,7 @@ def equationroots(a, b, c):
   # If a is 0, then equation is not quadratic, but Liner
   if a == 0:
     print("Liner Equation:")
-    print("Root is: {}".format(-c/b))
+    print("Root is: {}".format(- c / b))
     return
 
   # calculating discriminate using formula
@@ -18,18 +18,18 @@ def equationroots(a, b, c):
   sql_val = math.sqrt(abs(dis))
 
   if dis > 0:
-    r1 = (-b + sql_val)/(2 * a)
-    r2 = (-b - sql_val)/(2 * a)
+    r1 = (- b + sql_val) / (2 * a)
+    r2 = (- b - sql_val) / (2 * a)
     print("Roots are real and distinct:")
     print("Root 1: {} Root 2: {}".format(r1, r2))
   ## root are real
   elif dis == 0:
-    r1 = r2 = -b / (2 * a)
+    r1 = r2 = - b / (2 * a)
     print("The roots are real and equal:")
     print("Root 1: {} Root 2: {}".format(r1, r2))
   # when discriminant is less than 0
   else:
-    r = -b/(2 * a)
+    r = - b / (2 * a)
     print("The roots are imaginary:")
     print("Roo1: {} + i{}".format(r, sql_val))
     print("Roo1: {} - i{}".format(r, sql_val))
@@ -41,14 +41,3 @@ if __name__ == "__main__":
   a, b, c = [int(x) for x in input("Enter the coefficients of x:").split()]
 
   equationroots(a, b, c)
-
-  ## three test case
-  print("some test cases:")
-  list = [(1, 1, 1), (1, 7, 12), (1, -2, 1)]
-  for item in list:
-    print("a={} b={} c={}".format(item[0], item[1], item[2]))
-    equationroots(item[0], item[1], item[2])
-
-
-
-
