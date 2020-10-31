956a3e7267fd55e13d916dbb99c624f9930749b7
vguo <vguo@us.ibm.com>
2020-10-31 16:57:37 -0400

    add sample output

--- 1st Year/05. Binary Search in 1D Array/Binary_search_in_1D_array.py
+++ 1st Year/05. Binary Search in 1D Array/Binary_search_in_1D_array.py
@@ -27,3 +27,31 @@ if __name__ == "__main__":
     print("Key is available at {} location.".format(result + 1))
   else:
     print("Key is not Found.")
+
+'''
+
+Sample output
+Enter the number of elements: 5
+Enter the Array in ascending order: 
+1
+2
+4
+6
+7
+[1, 2, 4, 6, 7]
+Enter the key to be searched: 4
+Key is available at 3 location.
+
+Enter the number of elements: 6
+Enter the Array in ascending order: 
+3
+5
+6
+8
+9
+13
+[3, 5, 6, 8, 9, 13]
+Enter the key to be searched: 13
+Key is available at 6 location.
+
+'''
