11ec2f1ff2632f4fbf4817b762aa78eaea469ff1
vguo <vguo@us.ibm.com>
2020-11-05 14:21:39 -0500

    update calculator based on code review

--- 1st Year/01. Basic Calculator/calculator.py
+++ 1st Year/01. Basic Calculator/calculator.py
@@ -1,35 +1,102 @@
-import operator
 ## To implement a calculator
 
-my_operators = {
-    '+': [operator.add, "Sum"],
-    '-': [operator.sub, "Difference"],
-    '*': [operator.mul, "Product"],
-    '/': [operator.truediv, "Quotient"],
-    '%': [operator.mod, "Remainder"]
-}
 
-def calculator(symbol,num1,num2):
+def Sum(num1, num2):
+    return "Sum = {}".format(num1 + num2)
 
-  # Find invalid operator
-  if symbol not in my_operators:
-    print("Invalid operator!!\n")
+
+def Difference(num1, num2):
+    return "Difference = {}".format(num1 - num2)
+
+
+def Product(num1, num2):
+    return "Product = {}".format(num1 * num2)
+
+
+def Quotient(num1, num2):
+    return "Quotient = {}".format(num1 / num2)
+
+
+def Remainder(num1, num2):
+    return "Remainder = {}".format(num1 % num2)
+
+
+my_operators = {"+": Sum, "-": Difference, "*": Product, "/": Quotient, "%": Remainder}
+
+
+def calculator(symbol, num1, num2):
+
+    # Find invalid operator
+    if symbol not in my_operators:
+        print("Invalid operator!!\n")
+        return
+    # Test condition cannot divide by zero
+    if int(num2) == 0 and (symbol in ["/", "%"]):
+        print("Cannot divide by zero!!\n")
+        return
+
+    print(my_operators.get(symbol)(int(num1), int(num2)))
     return
-  # Test condition cannot divide by zero
-  if int(num2) == 0 and (symbol in ["/", "%"]):
-    print("Cannot divide by zero!!\n")
-    return
-  result = my_operators.get(symbol)[0](int(num1), int(num2))
-  print("{0} = {1}".format(my_operators.get(symbol)[1], result))
-
 
 if __name__ == "__main__":
-  print("Menu:")
-  print("Addition\n- Subtraction\n* Multiplication\n/ Division\n% Remainder\n")
-  symbol = input("Enter the symbol for operation:")
-  print(symbol)
-  num1 = input("Enter the first number: ")
-  num2 = input("Enter the second number: ")
-  print(num1, num2)
+    print("Menu:")
+    print("+ Addition\n- Subtraction\n* Multiplication\n/ Division\n% Remainder\n")
+    symbol = input("Enter the symbol for operation:")
+    print(symbol)
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+    print(num1, num2)
 
-  calculator(symbol, num1, num2)
+    ## Test for each case +, -, *, /, %
+    calculator(symbol, num1, num2)
+
+
+"""
+Enter the symbol for operation:\
+\
+Enter the first number: 3
+Enter the second number: 4
+3 4
+Invalid operator!!
+
+Sample output
+Enter the symbol for operation: ----> +  
+Enter the first number: ---->3
+Enter the second number:---->4
+3 4
+Sum = 7
+
+Enter the symbol for operation: ----> -  
+Enter the first number: ---->5
+Enter the second number:---->4
+5 4
+Difference = 1
+
+Enter the symbol for operation:*
+*
+Enter the first number: 4
+Enter the second number: 5
+4 5
+Product = 20
+
+Enter the symbol for operation:/
+/
+Enter the first number: 7
+Enter the second number: 5
+7 5
+Quotient = 1.4
+
+Enter the symbol for operation:%
+%
+Enter the first number: 20
+Enter the second number: 3
+20 3
+Remainder = 2
+
+Enter the symbol for operation:/
+/
+Enter the first number: 3
+Enter the second number: 0
+3 0
+Cannot divide by zero!!
+"""
